babae08f217500f9c9984d7cead8822c1833ed49
Bodin Virgile <virgile.bodin@etu.univ-orleans.fr>
2025-09-25 23:05:52 +0200

    ajout menu de livre et vue d'un livre

--- tp2/bonnes_lectures/urls.py
+++ tp2/bonnes_lectures/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('about/', views.about, name='about'),
-  path('welcome/', views.welcome, name='welcome'),
+    path("", views.index, name="index"),
+    path("book/<int:book_id>/", views.book, name="book"),
 ]
--- tp2/bonnes_lectures/views.py
+++ tp2/bonnes_lectures/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from bonnes_lectures.models import *
 
-def about(request):
-  return HttpResponse("Bienvenue dans l'application 'Bonnes lectures'")
 
-def welcome(request):
-  return render(request, 'bonnes_lectures/welcome.html')
+def index(request):
+  books = Book.objects.order_by("-year")
+  context = {"all_books": books}
+  return render(request, "bonnes_lectures/books.html", context)
+
+def book(request, book_id):
+  try:
+    book = Book.objects.get(pk=book_id)
+  except Book.DoesNotExist:
+    raise Http404("Le livre n'existe pas")
+  return render(request, "bonnes_lectures/book.html", {"book":book})
